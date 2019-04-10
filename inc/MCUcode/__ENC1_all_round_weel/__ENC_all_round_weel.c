0ce755dfcf5cf1e4df823a321944045414a04644
rangeccc <rangeccc@163.com>
2019-04-10 23:43:00 +0800

    添加——__ENC1_三轮全向轮

---
+++ inc/MCUcode/__ENC1_all_round_weel/__ENC_all_round_weel.c
@@ -0,0 +1,26 @@
+#include "__ENC_all_round_weel.h"
+
+//			signed short int				s16
+//			unsigned short int			u16
+//			unsigned char						u8
+
+void __ENC1_Encoder_motor(unsigned char Motor,signed short int Speed)
+{
+//EXTERNAL_Input1(a,b);
+}
+	/*********************************/
+
+
+
+/*********************************/
+void __ENC1_Set_Car(unsigned short int angle,signed short int Speed)
+{
+	
+}
+
+
+
+
+
+
+
---
+++ inc/MCUcode/__ENC1_all_round_weel/__ENC_all_round_weel.h
@@ -0,0 +1,7 @@
+#ifndef __ENC_all_round_weel_H
+#define __ENC_all_round_weel_H
+
+
+
+
+#endif
